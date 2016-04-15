5aba0b95411be376b3ee8a89af887dde0da410de
Alex <turnerawuindy@gmail.com>
2016-04-14 20:25:47 -0400

    first update

--- racecar.cpp
+++ racecar.cpp
@@ -1,14 +1,19 @@
 // This progam calcuates how fast a driver is going in MPH.
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() 
 {
+  ofstream outputFile;
 // Constant
 const double MILES = 2.5;
   
 //setting up variables
 double mph, speed, sec;
+  
+// Open an output file.
+outputFile.open("racecar.txt");
 
 // Get how fast a lap was finished.
 cout << "How fast did the driver finish the lap in seconds? ";
@@ -21,7 +26,11 @@ speed = (sec / 60);
 mph = ((MILES * 60) / speed);
 
 // Display length of lap.
-cout << "The driver is going "; 
-cout << mph << "mph.\n";
+outputFile << "The driver is going "; 
+outputFile << mph << "mph.\n";
+  
+// Close the file.
+outputFile.close();
+cout << "The information has been saved to a file.\n";
 return 0;
 }
